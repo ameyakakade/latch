53dea5b8d8089bb0fcf2dcf9f409e4ae71ef04a0
Lakshya Gupta <lakshyaranu@gmail.com>
2025-07-18 02:40:22 +0530

    feat: login and get started page

--- app/src/main/java/com/vinnovateit/autonetconnector/MainActivity.kt
+++ app/src/main/java/com/vinnovateit/autonetconnector/MainActivity.kt
@@ -2,46 +2,18 @@ package com.vinnovateit.autonetconnector
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.vinnovateit.autonetconnector.ui.theme.AutoNetConnectorTheme
+import android.content.Intent
+import android.widget.Button
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            AutoNetConnectorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.landing_page)
+
+        val getStartedButton = findViewById<Button>(R.id.get_started_button)
+        getStartedButton.setOnClickListener {
+            val intent = Intent(this, NewPageActivity::class.java)
+            startActivity(intent)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AutoNetConnectorTheme {
-        Greeting("Android")
     }
 }
